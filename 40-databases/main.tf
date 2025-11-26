30d8defdfac20af17b332bc2a88a9ab7ea05da25
Jayani22 <mutcherlajayani@gmail.com>
2025-11-26 10:14:23 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -24,7 +24,7 @@ resource "terraform_data" "mongodb" {
         host = aws_instance.mongodb.private_ip
     }
 
-    provisioner = "remote-exec" {
+    provisioner "remote-exec" {
         inline = [
             "echo Hello World"
         ]
