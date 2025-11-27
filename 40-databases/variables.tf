c443c96120810172b78bcdedabb92c38f9dffc08
Jayani22 <mutcherlajayani@gmail.com>
2025-11-27 10:31:35 +0530

    terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,28 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        # frontend-load-balancer
+        "frontend_alb",
+        # Backend ALB
+        "backend_alb"
+    ]
+}
+
+variable "zone_id" {
+    default = "Z08769191SZ0EERXMPHL9"
+}
+
+variable "domain_name" {
+    default = "jayani23.fun"
+}
--- 60-catalogue/catalogue.sh
+++ 60-catalogue/catalogue.sh
@@ -3,10 +3,6 @@
 component=$1
 environment=$2
 dnf install ansible -y
-# ansible-pull -U https://github.com/Jayani22/ansible-roboshop-roles-tf.git -e component=$component main.yaml
-# git clone ansible-playbook
-# cd ansible-playbook
-# ansible-playbook -i inventory.ini main.yaml
 
 REPO_URL=https://github.com/Jayani22/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
