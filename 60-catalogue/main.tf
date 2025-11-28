7eaa9790f5a56f523588a965736961423dcae3c2
Jayani22 <mutcherlajayani@gmail.com>
2025-11-28 11:08:52 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -156,7 +156,7 @@ resource "aws_autoscaling_group" "catalogue" {
 
 }
 
-resource "aws_autoscaling_policy" "catalogue" {
+resource "aws_autoscaling_policy" "example" {
     autoscaling_group_name = aws_autoscaling_group.catalogue.name
     name = "${local.common_name_suffix}-catalogue"
     policy_type = "TargetTrackingScaling"
