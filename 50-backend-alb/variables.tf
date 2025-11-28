9f437c42a0bb99ff1dd69d6f0d9f896600b5d953
Jayani22 <mutcherlajayani@gmail.com>
2025-11-28 12:03:33 +0530

    terraform

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z08769191SZ0EERXMPHL9"
+}
+
+variable "domain_name" {
+    default = "jayani23.fun"
+}
